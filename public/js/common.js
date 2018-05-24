2ee8887aeeb194ed5fcf914129ee95f0c275012f
WJNmm <2427372199@qq.com>
2018-05-24 21:24:20 +0800

    完成退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,26 @@
+NProgress.start();
 
-	NProgress.start();
+NProgress.done();
 
-	NProgress.done();
+$('.navs ul').prev('a').on('click', function () {
+    $(this).next().slideToggle();
+});
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+//退出功能
+$('#logoutBtn').click(function () {
+    console.log(123);
+    $.ajax({
+        type: 'post',
+        url: 'api/logout',
+        dataType: 'json',
+        success: function (data) {
+            if (data.code == 200) {
+                location.href = '/main/login';
+            }
+        }
+    });
+
+});
+
+
+
