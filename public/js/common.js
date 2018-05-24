8bf30de6c420b7fd67295993d4513ad5f1ba247b
WJNmm <2427372199@qq.com>
2018-05-25 01:48:01 +0800

    项目模块化重构

--- public/js/common.js
+++ public/js/common.js
@@ -1,26 +1,21 @@
-NProgress.start();
-
-NProgress.done();
-
-$('.navs ul').prev('a').on('click', function () {
-    $(this).next().slideToggle();
-});
-
-//退出功能
-$('#logoutBtn').click(function () {
-    console.log(123);
-    $.ajax({
-        type: 'post',
-        url: 'api/logout',
-        dataType: 'json',
-        success: function (data) {
-            if (data.code == 200) {
-                location.href = '/main/login';
-            }
-        }
+define(['jquery'],function($){
+    //NProgress.start();
+    //NProgress.done();
+    //控制左侧菜单的折叠和展开
+    $('.navs ul').prev('a').on('click', function () {
+        $(this).next().slideToggle();
     });
+    //退出功能
+    $('#logoutBtn').click(function(){
+        $.ajax({
+            type:'post',
+            url:'/api/logout',
+            success:function(data) {
+                if(data.code == 200) {
+                    location.href = './main/login'
+                }
+            }
 
+        });
+    })
 });
-
-
-
