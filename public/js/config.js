c52b6dafccb4b1a76fb748a968886b412865fbb0
WJNmm <2427372199@qq.com>
2018-05-25 17:18:58 +0800

    讲师查看功能

--- public/js/config.js
+++ public/js/config.js
@@ -1,12 +1,17 @@
 require.config({
     baseUrl: '/public/assets',
-    paths:{
+    paths: {
         jquery: 'jquery/jquery',
-        cookie : 'jquery-cookie/jquery.cookie',
-        template : 'artTemplate/template-web',
+        cookie: 'jquery-cookie/jquery.cookie',
+        template: 'artTemplate/template-web',
+        bootstrap: 'bootstrap/js/bootstrap.min',
         common: '../js/common',
         login: '../js/login',
-        teacherlist : '../js/teacher-list',
-
+        teacherlist: '../js/teacher-list',
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        }
     }
 });
