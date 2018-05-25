5d497cc73efb43a148f8711f7ebc5621c8caaae5
WJNmm <2427372199@qq.com>
2018-05-25 13:32:13 +0800

    模版引擎重构登录功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($){
+define(['jquery', 'template', 'cookie'], function ($, template) {
     //NProgress.start();
     //NProgress.done();
     //控制左侧菜单的折叠和展开
@@ -6,12 +6,12 @@ define(['jquery','cookie'],function($){
         $(this).next().slideToggle();
     });
     //退出功能
-    $('#logoutBtn').click(function(){
+    $('#logoutBtn').click(function () {
         $.ajax({
-            type:'post',
-            url:'/api/logout',
-            success:function(data) {
-                if(data.code == 200) {
+            type: 'post',
+            url: '/api/logout',
+            success: function (data) {
+                if (data.code == 200) {
                     location.href = './main/login'
                 }
             }
@@ -20,17 +20,20 @@ define(['jquery','cookie'],function($){
     });
     // 验证是否登录
     var sessionId = $.cookie('PHPSESSID');
-    if(!sessionId && location.pathname != '/main/login'){
+    if (!sessionId && location.pathname != '/main/login') {
         // sessionID不存在重新跳转到登录页面
         location.href = '/main/login';
     }
 
     // 获取登录信息
     var loginInfo = $.cookie('loginInfo');
-    var info = loginInfo?JSON.parse(loginInfo):{};
+    var info = loginInfo ? JSON.parse(loginInfo) : {};
     console.log(info);
 
-   
-     $('.profile img').attr('src',info.tc_avatar);
-     $('.profile h4').html(info.tc_name);
+    var tplstr = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+    var html = template.render(tplstr, info);
+    $('.aside .profile').html(html);
+
+    //$('.profile img').attr('src',info.tc_avatar);
+    //$('.profile h4').html(info.tc_name);
 });
