719a421af02c2ca3e6096ac7ae90bb1e231b2c37
WJNmm <2427372199@qq.com>
2018-05-26 00:24:44 +0800

    添加表单验证插件

--- public/js/config.js
+++ public/js/config.js
@@ -3,23 +3,26 @@ require.config({
     paths: {
         jquery: 'jquery/jquery',
         cookie: 'jquery-cookie/jquery.cookie',
-        template: 'artTemplate/template-web',
-        bootstrap: 'bootstrap/js/bootstrap.min',
-        datepicker : 'bootstrap-datepicker/js/bootstrap-datepicker',
-        language : 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+        template:'artTemplate/template-web',
+        bootstrap:'bootstrap/js/bootstrap.min',
+        datepicker:'bootstrap-datepicker/js/bootstrap-datepicker',
+        language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+        validate:'validate/jquery-validate.min',
         common: '../js/common',
         login: '../js/login',
-        index: '../js/index',
+        index:'../js/index',
         teacherlist: '../js/teacher-list',
         teacheradd: '../js/teacher-add',
-        util : '../js/util'
-    },
-    shim: {
-        bootstrap: {
-            deps: ['jquery']
+        util:'../js/util'
+    },shim:{
+        bootscript:{
+            deps:['jquery']
         },
-        language : {
-            deps : ['jquery','datepicker']
+        language:{
+            deps:['jquery','datepicker']
+        },
+        validate:{
+            deps:['jquery']
         }
     }
 });
