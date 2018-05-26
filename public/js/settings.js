afabc4e446c5a6b88fdbeeb84c738809c0df2922
WJNmm <2427372199@qq.com>
2018-05-26 14:43:27 +0800

    完成省市县三级联动功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,30 +1,34 @@
-define(['jquery','template','util','uploadify'],function($,template,util){
+define(['jquery', 'template', 'util', 'uploadify', 'datepicker', 'language', 'region'], function ($, template, util) {
     //设置导航菜单选中
     util.setMenu('/main/index');
     //调用后台接口填充表单
     $.ajax({
-        type:'get',
-        url:'/api/teacher/profile',
-        dataType:'json',
-        success:function(data){
+        type: 'get',
+        url: '/api/teacher/profile',
+        dataType: 'json',
+        success: function (data) {
             //console.log(data);
-            var html = template('settingsTpl',data.result);
+            var html = template('settingsTpl', data.result);
             $('#settingsInfo').html(html);
             //处理头像上传
             $('#upfile').uploadify({
-                width:120,
-                height:120,
-                buttonText:'',
-                itemTemplate:'<span></span>',
-                swf:'/public/assets/uploadify/uploadify.swf',
-                uploader:'/api/uploader/avatar',
-                fileObjName :'tc_avatar',
-                onUploadSuccess:function(f,data) {
+                width: 120,
+                height: 120,
+                buttonText: '',
+                itemTemplate: '<span></span>',
+                swf: '/public/assets/uploadify/uploadify.swf',
+                uploader: '/api/uploader/avatar',
+                fileObjName: 'tc_avatar',
+                onUploadSuccess: function (f, data) {
                     var data = JSON.parse(data);
                     //重置头像的图片地址
-                    $('.preview img').attr('src',data.result.path);
+                    $('.preview img').attr('src', data.result.path);
                 }
             });
+            //处理省市县三级联动
+            $('#pcd').region({
+                url: '/public/assets/jquery-region/region.json'
+            })
         }
     });
 });
