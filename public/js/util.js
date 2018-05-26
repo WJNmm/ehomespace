9b0d3b264a61d655d9c153e89c5532c280954b75
WJNmm <2427372199@qq.com>
2018-05-26 23:07:21 +0800

    课程列表展示功能

--- public/js/util.js
+++ public/js/util.js
@@ -1,8 +1,8 @@
 define(['jquery'], function ($) {
     //工具函数
     return {
-        setMenu: function (path) {//设置导航菜单选中
-            $('.navs a[href="' + path + '"]').addClass('active');
+        setMenu: function (path) {// 设置导航菜单选中
+            $('.navs a[href="'+path+'"]').addClass('active').closest('ul').show();
         },
         //
         qs: function (key) {//获取指定的URL值
@@ -10,11 +10,11 @@ define(['jquery'], function ($) {
             var param = location.search.substring(1);
             var result = null;
             //console.dir(param);
-            if(param) {
+            if (param) {
                 var kvs = param.split('&');
-                $.each(kvs,function(i,item){
+                $.each(kvs, function (i, item) {
                     var kv = item.split('=');
-                    if(key == kv[0]){
+                    if (key == kv[0]) {
                         result = kv[1];
                         return false;//终止循环
                     }
