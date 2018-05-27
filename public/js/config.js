ec8bc3a57c5c63bfb64cfa69b57dfb61ca20e806
WJNmm <2427372199@qq.com>
2018-05-27 13:50:23 +0800

    课程模块初步完成

--- public/js/config.js
+++ public/js/config.js
@@ -13,6 +13,7 @@ require.config({
         region: 'jquery-region/jquery.region',
         ckeditor:'ckeditor/ckeditor',
         nprogress:'nprogress/nprogress',
+        jcrop : 'jcrop/js/Jcrop',
         common: '../js/common',
         login: '../js/login',
         index: '../js/index',
